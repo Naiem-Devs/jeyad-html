d39a975a5d0f44f90602e0d1ca3241eaa77b97cc
Naiem-Devs <dnsnaiem@gmail.com>
2025-03-18 21:04:54 +0600

    update-file

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,51 +16,6 @@
   body.classList.toggle("overflow-hidden")
  })
 
-  (function () {
-    const second = 1000,
-          minute = second * 60,
-          hour = minute * 60,
-          day = hour * 24;
-  
-    //I'm adding this section so I don't have to keep updating this pen every year :-)
-    //remove this if you don't need it
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "04/26/",
-        birthday = dayMonth + yyyy;
-    
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-      birthday = dayMonth + nextYear;
-    }
-    //end
-    
-    const countDown = new Date(birthday).getTime(),
-        x = setInterval(function() {    
-  
-          const now = new Date().getTime(),
-                distance = countDown - now;
-  
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-          //do something later when date is reached
-          if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my birthday!";
-            document.getElementById("countdown").style.display = "none";
-            document.getElementById("content").style.display = "block";
-            clearInterval(x);
-          }
-          //seconds
-        }, 0)
-    }());
-
-
 
     var swiper = new Swiper(".wetherSwiper", {
       slidesPerView: 2,
@@ -92,8 +47,8 @@
       slidesPerView: 2,
       spaceBetween: 20,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-news-next",
+        prevEl: ".swiper-news-prev",
       },
       breakpoints: {
         340: {
@@ -151,8 +106,8 @@
       slidesPerView: 1,
       spaceBetween: 0,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-Moments-next",
+        prevEl: ".swiper-Moments-prev",
       },
       thumbs: {
         swiper: swiper,
@@ -164,8 +119,37 @@
       slidesPerView: 2,
       spaceBetween: 20,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-blog-next",
+        prevEl: ".swiper-blog-prev",
+      },
+      breakpoints: {
+        340: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 2,
+        },
+        1200: {
+          slidesPerView: 2,
+        },
+        1500: {
+          slidesPerView: 2,
+        },
+      },
+    });
+
+    var swiper = new Swiper(".blogsSwiper", {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".swiper-blogs-next",
+        prevEl: ".swiper-blogs-prev",
       },
       breakpoints: {
         340: {
